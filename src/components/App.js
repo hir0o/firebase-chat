009ad9b37dc7686985fb6633b0cd04beb96510dc
hir0o <mk.9.q.kyu@gmail.com>
2021-04-01 18:47:26 +0900

    update sigunin form

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,15 @@
 import { useState } from "react";
 import "./App.css";
 import SignIn from "./SignIn";
+import Main from "./Main";
 
 function App() {
   const [name, setName] = useState("");
-  console.log(name);
-  return <SignIn setName={setName} />;
+  if (name === "") {
+    return <SignIn setName={setName} />;
+  } else {
+    return <Main name={name} />;
+  }
 }
 
 export default App;
---
+++ src/components/Main.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Main = ({ name }) => {
+  return <div>name: {name}</div>;
+};
+
+export default Main;
